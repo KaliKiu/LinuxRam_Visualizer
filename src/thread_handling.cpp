26d7469f7b76666481d0d44a1f26c50fa17484ea
KaliKiu <palablink@gmail.com>
2025-09-08 00:17:04 +0200

    debugging

--- src/thread_handling.cpp
+++ src/thread_handling.cpp
@@ -41,6 +41,8 @@ namespace Thread{
                         });
         fetch_pid_data.detach();
         //while(true){std::cout<<"a"<<std::endl;std::this_thread::sleep_for(std::chrono::seconds(1));}
+        
+        //debugging stuff ahead!!!
         int count=0;
         while(true){printf("\nCOUNT: %d\nMemTotal: %d\nMemFree: %d\n",count,
                     data->meminfo_struct->memtotal,data->meminfo_struct->memfree);
@@ -48,9 +50,17 @@ namespace Thread{
             {   
                 auto VPage_map_ptr = data->VPage_map;
                 std::lock_guard<std::mutex> lock(VPage_map_mutex);
+                
+                /*auto pidVpage = *data->VPage_map->begin()->second;
+                if(pidVpage.empty())std::cout<<"empty Vector"<<std::endl;continue;
+                for(int i{0}; i<pidVpage.size();i++){
+                    if(!pidVpage[i])std::cout<<"null"<<std::endl;continue;
+                    printf("%lx-%lx name: %s",pidVpage[i]->start_Vaddr,pidVpage[i]->end_Vaddr,pidVpage[i]->path_name);
+                }*/
                 for(auto &t : *data->VPage_map){
                     if(!t.second || t.second->empty()) continue;
                     printf("\n%d: %lx-%lx",t.first,(*t.second)[0]->start_Vaddr,(*t.second)[0]->end_Vaddr);
+                    
                 }
             }
         std::this_thread::sleep_for(std::chrono::seconds(1));
