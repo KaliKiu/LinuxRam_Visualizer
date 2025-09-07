b7702ce3c2a4188d3f7e6861415a0172d682f357
KaliKiu <palablink@gmail.com>
2025-09-07 23:55:36 +0200

    segmentation fault fixed, parsing works

--- src/thread_handling.cpp
+++ src/thread_handling.cpp
@@ -26,7 +26,8 @@ namespace Thread{
                             std::vector<std::string> pids = Data::getPid();
                             int count = 0;
                             std::vector<std::thread> threads;
-                            std::lock_guard<std::mutex> lock(VPage_map_mutex);
+                            {
+                                std::lock_guard<std::mutex> lock(VPage_map_mutex);
                             for(const std::string &pid : pids){
                                 threads.emplace_back([&data,&pid, count]{
                                     data->parsePidMap(pid,count);
@@ -35,11 +36,12 @@ namespace Thread{
                             for(auto &t : threads) t.join();
 
                             std::this_thread::sleep_for(std::chrono::seconds(1));
+                            }
                         }
                         });
         fetch_pid_data.detach();
-        while(true){std::cout<<"a"<<std::endl;std::this_thread::sleep_for(std::chrono::seconds(1));}
-        /*int count=0;
+        //while(true){std::cout<<"a"<<std::endl;std::this_thread::sleep_for(std::chrono::seconds(1));}
+        int count=0;
         while(true){printf("\nCOUNT: %d\nMemTotal: %d\nMemFree: %d\n",count,
                     data->meminfo_struct->memtotal,data->meminfo_struct->memfree);
                     count++;
@@ -47,10 +49,11 @@ namespace Thread{
                 auto VPage_map_ptr = data->VPage_map;
                 std::lock_guard<std::mutex> lock(VPage_map_mutex);
                 for(auto &t : *data->VPage_map){
-                    printf("\n%d: 0x-0x",t.first,(*t.second)[0]->start_Vaddr,(*t.second)[0]->end_Vaddr);
+                    if(!t.second || t.second->empty()) continue;
+                    printf("\n%d: %lx-%lx",t.first,(*t.second)[0]->start_Vaddr,(*t.second)[0]->end_Vaddr);
                 }
             }
         std::this_thread::sleep_for(std::chrono::seconds(1));
-    }*/
+    }
 }
 }
